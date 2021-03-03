6853cc93ce6a3a9da8107f65e71ef5f3e276d677
Lori Winston <Lcwinston@gmail.com>
2021-03-03 08:58:47 -0800

    submission

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+import Header from './components/Header.js';
+import PrivateRoute from './components/PrivateRoute.js';
+import Home from './Home/Home.js';
+import SignUpPage from './AuthPages/SignUpPage.js';
+import LoginPage from './AuthPages/LoginPage.js';
+import TodosListPage from './TodosListPage/TodosListPage.js';
+import { getUserFromLocalStorage, putUserInLocalStorage } from './local-storage-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+      user: getUserFromLocalStorage()
+    }
+
+    handleUserChange = (user) => {
+      this.setState({ user })
+      
+      putUserInLocalStorage(user);
+    }
+
+    handleLogout = () => {
+      this.handleUserChange();
+    }
+
+    render() {
+      const { user } = this.state;
+        return (
+            <div>
+                <Router>
+                  <Header
+                    user={this.state.user}
+                    handleLogout={this.handleLogout}/>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <Home {...routerProps} />} 
+                        />
+                        <PrivateRoute 
+                            path="/todos" 
+                            exact
+                            token={user && user.token}
+                            render={(routerProps) => 
+                              <TodosListPage 
+                                user={this.state.user}
+                                {...routerProps} 
+                              />} 
+                        />
+                        <Route 
+                          path="/login" 
+                          exact
+                          render={(routerProps) => 
+                            <LoginPage 
+                              handleUserChange={this.handleUserChange}
+                              {...routerProps} 
+                            />} 
+                        />
+                        <Route 
+                          path="/signup" 
+                          exact
+                          render={(routerProps) => 
+                            <SignUpPage 
+                              handleUserChange={this.handleUserChange}
+                              {...routerProps} 
+                            />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
